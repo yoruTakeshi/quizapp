1e6a2b540d2e8285acc15ab7fd6c377e8ee87b96
sugoiTakeshi <marcus.silva@eaportal.org>
2024-06-06 02:54:41 +0000

    adc js do quiz para troca de tema

---
+++ pages/quiz/quiz.js
@@ -0,0 +1,11 @@
+import { verificarTema, trocarTema } from "../../helpers/tema-helper.js"
+
+const botaoTema = document.querySelector(".tema button")
+const body = document.querySelector("body")
+
+botaoTema.addEventListener("click", () => {
+    trocarTema(tema, body, botaoTema)
+})
+
+
+verificarTema(tema, body, botaoTema)
