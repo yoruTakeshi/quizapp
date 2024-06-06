c730114f988e547d5bb8924e961309d2ec5f52a6
sugoiTakeshi <marcus.silva@eaportal.org>
2024-06-06 05:53:26 +0000

    ⁠Meu nome é Yoshikage Kira. Tenho 33 anos. Minha casa fica na parte nordeste de Morioh, onde todas as casas estão, e eu não sou casado. Eu trabalho como funcionário das lojas de departamentos Kame Yu e chego em casa todos os dias às oito da noite, no máximo. Eu não fumo, mas ocasionalmente bebo. Estou na cama às 23 horas e me certifico de ter oito horas de sono, não importa o que aconteça. Depois de tomar um copo de leite morno e fazer cerca de vinte minutos de alongamentos antes de ir para a cama, geralmente não tenho problemas para dormir até de manhã. Assim como um bebê, eu acordo sem nenhum cansaço ou estresse pela manhã. Foi-me dito que não houve problemas no meu último check-up. Estou tentando explicar que sou uma pessoa que deseja viver uma vida muito tranquila. Eu cuido para não me incomodar com inimigos, como ganhar e perder, isso me faria perder o sono à noite. É assim que eu lido com a sociedade e sei que é isso que me traz felicidade. Embora, se eu fosse lutar, não perderia para ninguém.

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -9,6 +9,7 @@ let pontos = 0
 let pergunta = 1
 let resposta = ""
 let idInputResposta = ""
+let respostaCorretaId
 
 botaoTema.addEventListener("click", () => {
     trocarTema(tema, body, botaoTema)
@@ -96,7 +97,7 @@ function montarPergunta(){
                 </label>
             </form>
 
-            <button>Enviar</button>
+            <button>Responder</button>
         </section>
         `
 }
@@ -109,6 +110,19 @@ function guardarResposta(evento) {
     // alert("uauuuu")
     resposta = evento.target.value
     idInputResposta = evento.target.id
+
+    const botaoEnviar = document.querySelector(".alternativas button")
+    botaoEnviar.addEventListener("click", validarResposta)
+}
+
+function validarResposta(){
+    if (resposta === quiz.questions[pergunta-1].answer){
+        document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "correta")
+        pontos = pontos + 1
+    } else {
+        document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "errada")
+        document.querySelector(`label[for='${respostaCorretaId}']`).setAttribute("id", "correta")
+    }
 }
 
 async function iniciar(){
@@ -119,6 +133,10 @@ async function iniciar(){
     const inputResposta = document.querySelectorAll(".alternativas input")
     inputResposta.forEach(input =>{
         input.addEventListener("click", guardarResposta)
+
+        if (input.value === quiz.questions[pergunta-1].answer){
+            respostaCorretaId = input.id
+        }
     })
 }
 
