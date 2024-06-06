2b98b5a7bbdca6ec731f901c11adb803b92b9d8e
sugoiTakeshi <marcus.silva@eaportal.org>
2024-06-06 17:44:35 +0000

    fool and cool

--- script.js
+++ script.js
@@ -19,7 +19,8 @@ botoesAssunto.forEach(botao => {
 
 function selecionarAssunto(evento){
     // alert("eventoooooo")
-    const assunto = evento.target.innerText
+    const classeBotao = evento.target.className
+    const assunto = document.querySelector(`.${classeBotao} span`).innerText
     localStorage.setItem("assunto", assunto)
     window.location.href = "./pages/quiz/quiz.html"
 }
