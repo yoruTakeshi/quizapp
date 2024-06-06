9b49ce5aadda5b20ce0a3b6e3d0d61edef2d53bc
sugoiTakeshi <marcus.silva@eaportal.org>
2024-06-06 04:26:17 +0000

    bunda bumda estou endoidando cometerei suicidio as 27:99 do dia 6 de junho

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -1,10 +1,25 @@
 import { trocarTema, verificarTema, tema, body } from "../../helpers/tema-helper.js"
 
 const botaoTema = document.querySelector(".tema button")
-const body = document.querySelector("body")
+// const body = document.querySelector("body")
 
 botaoTema.addEventListener("click", () => {
     trocarTema(tema, body, botaoTema)
 })
 
 verificarTema(tema, body, botaoTema)
+
+const assunto = localStorage.getItem("assunto")
+
+
+function alterarAssunto(){
+    
+    const divicone = document.querySelector(".assunto_icone")
+    const iconeImg = document.querySelector(".assunto_icone img")
+    const assuntoTitulo = document.querySelector(".assunto h1")
+    divicone.classList.add(assunto.toLowerCase())
+    iconeImg.setAttribute("src", `../../assets/images/icon-${assunto.toLowerCase()}.svg`)
+    assuntoTitulo.innerText = assunto
+}
+
+alterarAssunto()
