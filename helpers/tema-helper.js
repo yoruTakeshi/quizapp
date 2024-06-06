3213e9681c2c28aecc3bfea4f0f2632b9a9a15cb
sugoiTakeshi <marcus.silva@eaportal.org>
2024-06-06 02:19:20 +0000

    criei helper do tema

---
+++ helpers/tema-helper.js
@@ -0,0 +1,30 @@
+let tema = "claro"
+
+export function trocarTema(tema, body, botaoTema){
+    // alert("trocadetema")
+    if (localStorage.getItem("tema")){
+        tema = localStorage.getItem("tema")
+    }
+    tema = localStorage.getItem("tema")
+
+    if (tema === "claro"){
+        body.classList.add("escuro")
+        localStorage.setItem("tema", "escuro")
+        botaoTema.style.justifyContent = "flex-end"
+    } else {
+        body.classList.remove("escuro")
+        localStorage.setItem("tema", "claro")
+        botaoTema.style.justifyContent = "flex-start"
+    }
+}
+
+export function verificarTema(){
+    if (localStorage.getItem("tema")){
+        tema = localStorage.getItem("tema")
+    }
+
+    if (tema === "escuro"){
+        body.classList.add("escuro")
+        botaoTema.style.justifyContent = "flex-end"
+    }
+}
--- script.js
+++ script.js
@@ -1,36 +1,11 @@
-let tema = "claro"
+import { trocarTema, verificarTema } from "./helpers/tema-helper.js"
 
 const botaoTema = document.querySelector(".tema button")
-botaoTema.addEventListener("click", trocarTema)
-
 const body = document.querySelector("body")
 
-function trocarTema(){
-    // alert("trocadetema")
-    if (localStorage.getItem("tema")){
-        tema = localStorage.getItem("tema")
-    }
-    tema = localStorage.getItem("tema")
+botaoTema.addEventListener("click", () => {
+    trocarTema(tema, body, botaoTema)
+})
 
-    if (tema === "claro"){
-        body.classList.add("escuro")
-        localStorage.setItem("tema", "escuro")
-        botaoTema 
-    } else {
-        body.classList.remove("escuro")
-        localStorage.setItem("tema", "claro")
-    }
-}
 
-function verificarTema(){
-    if (localStorage.getItem("tema")){
-        tema = localStorage.getItem("tema")
-    }
-
-    if (tema === "escuro"){
-        body.classList.add("escuro")
-
-    }
-}
-
-verificarTema()
+verificarTema(tema, body, botaoTema)
